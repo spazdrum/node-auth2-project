40c82d4dfcaec78b2cfe7326698e1fd7a9182158
spazdrum <spazdrum@gmail.com>
2020-10-02 14:41:10 -0400

    mvp

--- api/server.js
+++ api/server.js
@@ -26,3 +26,29 @@ server.post("/signup", (req, res) => {
       res.status(500).json({ message: `Server Error! ${err}` });
     });
 });
+
+server.post("/login", (req, res) => {
+  db.findByUsername(req.body.username)
+    .then((user) => {
+      if (user && bcrypt.compareSync(req.body.password, user[0].password)) {
+        res
+          .status(200)
+          .json({ message: "Login Successful!", token: jwt(user) });
+      } else {
+        res.status(401).json({ message: "Login Failed!" });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({ message: `Server Error! ${err}` });
+    });
+});
+
+server.get("/users", (req, res) => {
+  db.find()
+    .then((rep) => {
+      res.status(200).json({ data: rep });
+    })
+    .catch((err) => {
+      res.status(500).json({ message: `Server Error! ${err}` });
+    });
+});
